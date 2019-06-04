5f16ee6e241b2b1cbd0ab1fede62400ed7de3107
RonaldoFM <43863628+RonaldoFM@users.noreply.github.com>
2019-06-04 18:39:04 -0300

    PrimeiroCommit

---
+++ src/store/reducers/repositories.js
@@ -0,0 +1,15 @@
+// REducer
+
+const INITIAL_STATE = {};
+
+//Actions
+
+export default function repositories(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+
+  console.log(action);
+  return state;
+}
